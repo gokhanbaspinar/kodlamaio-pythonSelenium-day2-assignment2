19f3f859fc0a15b6dfd632af71f325c250de4a17
gokhanbaspinar <gkhnbspnr@gmail.com>
2023-03-14 18:37:08 +0300

    first commit

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Banka:
+    def krediBasvur(self):
+        print("Kredi başvurusu yapıldı")
+    
+    def krediHesapla(self):
+        print("Hesaplar yapıldı")
+
+banka = Banka()
+banka.krediBasvur()
+
+# self = this
---
+++ matematik.py
@@ -0,0 +1,17 @@
+class Matematik:
+    def __init__(self, sayi1, sayi2):
+        self.s1 = sayi1
+        self.s2 = sayi2
+    def topla(self):
+        return self.s1 + self.s2
+    def cikar(self):
+        return self.s1 - self.s2
+    def bol(self):
+        return self.s1 / self.s2
+    def carp(self):
+        return self.s1 * self.s2
+    
+matematik = Matematik(14, 7)
+sonuc = matematik.bol()
+print("Sonuç: " + str(sonuc))
+        
